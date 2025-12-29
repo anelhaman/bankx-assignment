789f1fa85d251ba7513f9d8b8b4e7dced583cd0b
Rachen Phengsang <rachen.phengsang@gmail.com>
2025-12-29 18:41:04 +0700

    Enable clean terraform destroy - allow deletion of resource groups with nested resources

--- main.tf
+++ main.tf
@@ -21,6 +21,9 @@ provider "azurerm" {
       delete_os_disk_on_deletion     = true
       skip_shutdown_and_force_delete = false
     }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
   }
 }
 
